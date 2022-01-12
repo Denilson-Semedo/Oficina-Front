20c6b8a0c507bf14d34d70c14a46efb35721886b
Denilson Semedo <dstavares00@gmail.com>
2022-01-11 23:33:29 -0100

    Função para gerar pdf com a libraria "itext"

--- src/oficina/TelaPrincipal.java
+++ src/oficina/TelaPrincipal.java
@@ -324,7 +324,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         resetColor(jPanel3);
         resetColor(jPanel4);
         resetColor(jPanel5);
-        new SwitchMenu(ContentPane, new Servicos());
+        //new SwitchMenu(ContentPane, new Servicos());
     }//GEN-LAST:event_jPanel2MouseClicked
 
     private void jPanel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseClicked
@@ -344,7 +344,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         resetColor(jPanel2);
         resetColor(jPanel3);
         resetColor(jPanel4);
-        new SwitchMenu(ContentPane, new Pecas());
+        //new SwitchMenu(ContentPane, new Pecas());
     }//GEN-LAST:event_jPanel5MouseClicked
 
     public void setColor (JPanel panel){
